51e7d97295966b8e5b717ed225860051ec53b255
M Landers <91501464+TheRealMaria@users.noreply.github.com>
2022-04-05 19:53:15 -0700

    added hw1

---
+++ jquery/script.js
@@ -0,0 +1,28 @@
+// im a java script
+
+// $('body').css('background','pink')
+
+// $('h1').click(function() {
+//         $('body').toggleClass('purple')
+//     }
+// )
+// let rainbow = 0
+// let colorVal = `hsl(${rainbow},100,100)`
+
+// $('button').click (function () {
+//     rainbow + 30
+//     $("body").css ('background',`${colorVal}`)
+// })
+
+$('.one').click(function () {
+ $('.kitties').attr('src', 'https://placekitten.com/401/401') 
+})
+
+$('.two').click(function () {
+    document.getElementsByClassName
+ $('.kitties').attr('src', 'https://placekitten.com/402/402')
+})
+
+$('.three').click(function () {
+ $('.kitties').attr('src', 'https://placekitten.com/403/403')
+})
