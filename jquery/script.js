da3a87b365b88efaf5aad83654b67b037cb23d49
M Landers <91501464+TheRealMaria@users.noreply.github.com>
2022-04-12 07:35:47 -0700

    jquery practice

--- jquery/script.js
+++ jquery/script.js
@@ -9,20 +9,46 @@
 // let rainbow = 0
 // let colorVal = `hsl(${rainbow},100,100)`
 
-// $('button').click (function () {
-//     rainbow + 30
-//     $("body").css ('background',`${colorVal}`)
-// })
+ $('button').click (function () {
+
+    $("body").css ('button',`${VarText} 'rude`)
+})
 
 $('.one').click(function () {
- $('.kitties').attr('src', 'https://placekitten.com/401/401') 
+ $('.kitties').attr('src', 'images/1.png') 
 })
 
 $('.two').click(function () {
     document.getElementsByClassName
- $('.kitties').attr('src', 'https://placekitten.com/402/402')
+ $('.kitties').attr('src', 'images/2.png')
 })
 
 $('.three').click(function () {
- $('.kitties').attr('src', 'https://placekitten.com/403/403')
+ $('.kitties').attr('src', 'images/3.png')
 })
+
+$('.four').click(function () {
+    $('.kitties').attr('src', 'images/4.png') 
+   })
+   
+   $('.five').click(function () {
+       document.getElementsByClassName
+    $('.kitties').attr('src', 'images/5.png')
+   })
+   
+   $('.six').click(function () {
+    $('.kitties').attr('src', 'images/6.png')
+   })
+
+   $('.seven').click(function () {
+    $('.kitties').attr('src', 'images/7.png') 
+   })
+   
+   $('.eight').click(function () {
+       document.getElementsByClassName
+    $('.kitties').attr('src', 'images/8.png')
+   })
+   
+   $('.nine').click(function () {
+    $('.kitties').attr('src', 'images/9.png')
+   })
